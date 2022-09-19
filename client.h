4d839a0c79b98bd57a5fac4c128ef00747d15eaf
Andrey529 <andr02eyes@gmail.com>
2022-09-19 19:34:20 +0300

    clear code

--- client.h
+++ client.h
@@ -19,77 +19,6 @@ namespace asio = boost::asio;
 class Client {
 public:
 
-//    void getResponse(const std::string &url) {
-//        boost::beast::error_code ec;
-//
-//        asio::io_context context;
-//
-//        asio::ip::tcp::endpoint endpoint(asio::ip::make_address(url, ec), 80);
-//
-//        asio::ip::tcp::socket socket(context);
-//        socket.connect(endpoint, ec);
-//
-//        if (!ec) {
-//            std::cout << "Connected " << ec.message() << std::endl;
-//        }
-//        else {
-//            std::cout << "Failed to connect to address:\n" << ec.message() << std::endl;
-//        }
-//
-//        if (socket.is_open()) {
-//            std::string sRequest =
-//                    "GET example.com\r\n";
-//
-//            socket.write_some(asio::buffer(sRequest.data(), sRequest.size()), ec);
-//
-//            using namespace std::chrono_literals;
-//            std::this_thread::sleep_for(400ms);
-//
-//            size_t bytes = socket.available();
-//            std::cout << "Bytes available: " << bytes << std::endl;
-//
-//            if (bytes > 0) {
-//                std::vector<char> vBuffer(bytes);
-//                socket.read_some(asio::buffer(vBuffer.data(), vBuffer.size()), ec);
-//
-//                for (auto c : vBuffer) {
-//                    std::cout << c;
-//                }
-//            }
-//        }
-//    }
-
-    // boost asio http get request function
-//    std::string getResponse(const std::string& host, const std::string& target) {
-//        boost::asio::io_context ioc;
-//        boost::asio::ip::tcp::resolver resolver(ioc);
-//        boost::beast::tcp_stream stream(ioc);
-//
-//        auto const results = resolver.resolve(host, "http");
-//
-//        stream.connect(results);
-//
-//        namespace http = boost::beast::http;
-//
-//        http::request<http::string_body> req{ http::verb::get, target, 11 };
-//        req.set(http::field::host, host);
-//        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
-//
-//        http::write(stream, req);
-//
-//        boost::beast::flat_buffer buffer;
-//        http::response<http::dynamic_body> res;
-//        http::read(stream, buffer, res);
-//
-//        boost::beast::error_code ec;
-//        stream.socket().shutdown(boost::asio::ip::tcp::socket::shutdown_both, ec);
-//
-//        if (ec && ec != boost::beast::errc::not_connected)
-//            throw boost::beast::system_error{ ec };
-//
-//        return boost::beast::buffers_to_string(res.body().data());
-//    }
-
     // boost asio hhtp get request function that get full url and return response in json in std::string
     std::string getResponse(const std::string& url) {
         boost::beast::error_code ec;
@@ -97,7 +26,6 @@ public:
         boost::asio::io_context ioc;
         boost::asio::ip::tcp::resolver resolver(ioc);
         boost::beast::tcp_stream stream(ioc);
-//        boost::beast::ssl_stream<boost::beast::tcp_stream> stream(ioc, boost::asio::ssl::context::sslv23_client);
 
         std::string host = url.substr(0, url.find('/'));
         std::string target = url.substr(url.find('/'));
@@ -160,13 +88,10 @@ public:
         boost::asio::io_context ioc;
         boost::asio::ssl::context ctx(boost::asio::ssl::context::sslv23);
         boost::asio::ip::tcp::resolver resolver(ioc);
-//        boost::asio::ssl::stream<asio::ip::tcp::socket> sock(ioc, ctx);
         boost::beast::ssl_stream<boost::beast::tcp_stream> stream(ioc, ctx);
 
         std::string host = url.substr(0, url.find('/'));
         std::string target = url.substr(url.find('/'));
-
-        std::cout << "host: " << host << "\ntarget: " << target << '\n';
 
         auto const results = resolver.resolve(host, "https", ec);
 
@@ -189,16 +114,14 @@ public:
         http::response<http::dynamic_body> res;
         http::read(stream, buffer, res);
 
-        std::cout << "data size: " << buffer.data().size() << '\n';
-
 
         switch(res.base().result_int()) {
             case 301:
-                std::cout << "Status code: " << res.result_int() << "\nRedirecting.....\n";
-                this->getResponse(res.base()["Location"].to_string().substr(res.base()["Location"].to_string().find("r")));
+//                std::cout << "Status code: " << res.result_int() << "\nRedirecting.....\n";
+                this->getResponseHttps(res.base()["Location"].to_string().substr(res.base()["Location"].to_string().find("r")));
                 break;
             case 200:
-                std::cout << "Status code: " << res.result_int() << '\n' << res << "\n";
+//                std::cout << "Status code: " << res.result_int() << '\n' << res << "\n";
                 break;
             default:
                 std::cout << "Unexpected HTTP status " << res.result_int() << "\n";
@@ -212,7 +135,6 @@ public:
             std::cerr << ec << '\n';
             throw boost::beast::system_error{ ec };
         }
-        std::cout << "Status code: " << res.result_int() << std::endl;
 
         return boost::beast::buffers_to_string(res.body().data());
     }
